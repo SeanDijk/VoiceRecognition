26269ed0ab9ae005a67e0dfbd86f873573274f5d
Dennis ter Maat <d.ter.maat@gmail.com>
2017-05-18 11:03:38 +0200

    Layout changes and images added.
    Added Wit.AI

---
+++ app/src/main/java/com/hhspls/voicerecognition/api/WitAi.java
@@ -0,0 +1,30 @@
+package com.hhspls.voicerecognition.api;
+
+import android.content.Context;
+
+/**
+ * Created by HICT-HP on 5/18/2017.
+ */
+
+public class WitAi extends AbstractApi {
+
+    public WitAi(Context context){
+        super(context);
+
+    }
+
+    @Override
+    void startListeningImpl() {
+
+    }
+
+    @Override
+    void stopListeningImpl() {
+
+    }
+
+    @Override
+    void cancelImpl() {
+
+    }
+}
